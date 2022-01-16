a5d02b140f9d764ce5d860d8be30a57b55c457b1
mhdyzrn <51892371+mhdyzrn@users.noreply.github.com>
2022-01-16 21:48:44 +0700

    Add files via upload

---
+++ cfg.php
@@ -0,0 +1,7 @@
+<?php
+
+#USER_AGENT
+$useragent = "Mozilla/5.0 (Linux; Android 9; Mi A1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.49 Mobile Safari/537.36";
+
+#COOKIE_SEASON_AUTOFAUCET
+$cookie = 'session_new=mhdyuzron@gmail.com; session_new_id=9f292d61c46ea525896f7ddd539cef42; _ga=GA1.1.1947159967.1641952788; HstCfa4531111=1641952788555; HstCmu4531111=1641952788555; HstCnv4531111=1; HstCns4531111=1; HstCla4531111=1641952810680; HstPn4531111=2; HstPt4531111=2; color_scheme=dark; session_ok=true; auto={%22email%22:%22mhdyuzron@gmail.com%22%2C%22coins%22:[%22bch%22%2C%22ltc%22%2C%22trx%22%2C%22dgb%22%2C%22doge%22%2C%22bnb%22%2C%22zec%22%2C%22dash%22%2C%22sol%22%2C%22fey%22]%2C%22mode%22:%22multi%22%2C%22boost%22:%225%22%2C%22payout_mode%22:%22fp%22}; _ga=GA1.1.1947159967.1641952788; _data_cpm=129-1; _data_cpc=69-1_77-1; _data_html=55-1_93-1_129-1; _ga_FGL3LGDBQR=GS1.1.1642295479.2.1.1642295779.0';
